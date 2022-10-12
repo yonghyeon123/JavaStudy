631788be6709a3e0abb6112f97193c28e8f7a813
yonghyeon123 <mnbvcx0229@gmail.com>
2022-10-12 17:59:35 +0900

    last commit

--- src/day1012/UserController.java
+++ src/day1012/UserController.java
@@ -1,6 +1,10 @@
 package day1012;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class UserController {
     private Connection connection;
@@ -9,7 +13,35 @@ public class UserController {
     private final String USERNAME = "root";
     private final String PASSWORD = "1111";
 
-    public void insert(){
+    public UserController(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(ADDRESS, USERNAME, PASSWORD);
+        }
+        catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void insert(UserDTO userDTO){
+        String query = "INSERT INTO `user` (`username`, `password`, `nickname`) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement pstmt = connection.prepareStatement(query);
+            pstmt.setString(1, userDTO.getUsername());
+            pstmt.setString(2, userDTO.getPassword());
+            pstmt.setString(3, userDTO.getNickname());
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<UserDTO> SelectAll(){
+        ArrayList<UserDTO> list = new ArrayList<>();
+
+        String query = "SELECT * FROM `user`";
+
+        return list;
     }
 }
